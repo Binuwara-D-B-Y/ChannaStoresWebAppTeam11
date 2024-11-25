257bcd24ef5d3766109c652f28e35ceb09f5a725
WMadaraChamudini <it23292154@my.sliit.lk>
2024-11-25 22:33:44 +0530

    updated

--- Backend/team11/src/main/java/com/example/team11/Repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.team11.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.team11.Entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-}
-
