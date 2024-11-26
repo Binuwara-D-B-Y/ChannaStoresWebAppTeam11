d8ef5e4f428bab7027a093b4615c0f1d4fc5d7bd
it23148840 <140532453+it23148840@users.noreply.github.com>
2024-11-27 01:23:20 +0530

    Delete Backend/team11 directory

--- Backend/team11/team11/src/main/java/com/example/team11/Repository/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.team11.Repository;
-
-import com.example.team11.Entity.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-}
-
