1e3673db80609015d63c86793dab0174b7367d4c
it23148840 <it23148840@my.sliit.lk>
2024-11-27 01:34:11 +0530

    updated

--- Backend/team11/team11/src/main/java/com/example/team11/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.team11.Repository;
-
-import com.example.team11.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email); // For login or lookup
-}
