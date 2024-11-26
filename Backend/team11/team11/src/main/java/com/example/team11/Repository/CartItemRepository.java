1236cac47d5f3216a5c9085d42ad7947b3f19c5c
Binuwara-D-B-Y <it23184558@my.sliit.lk>
2024-11-27 02:13:36 +0530

    Recreate Frontend and Backend directories and add new files

--- Backend/team11/team11/src/main/java/com/example/team11/Repository/CartItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.team11.Repository;
-
-import com.example.team11.Entity.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
-
