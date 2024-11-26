bea711fd0a1d3c377217ac68b8bda3e47365a6eb
H.M.J.D.HERATH <140620460+ja390@users.noreply.github.com>
2024-11-26 20:40:45 +0530

    Add files via upload

--- Backend/team11/team11/team11/src/main/java/com/example/team11/Entity/Product.java
+++ Backend/team11/team11/team11/src/main/java/com/example/team11/Entity/Product.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Table;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @Column(nullable = false)
     private String name;
@@ -27,11 +27,11 @@ public class Product {
     private String category;
 
     // Getters and Setters
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
